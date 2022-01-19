202ee863c591dd05ee84994789b656f6acd71e78
dhythm <yuta.okada.20@gmail.com>
2022-01-19 13:08:17 +0900

    install @mui/x-data-grid

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 export const Page: FC = ({ children }) => {
   return (
-    <Box height={"100vh"} bgcolor={"#282c34"}>
+    <Box height={"100vh"} bgcolor={"whitesmoke"}>
       <Box display={"flex"} flex={1} p={2}>
         {children}
       </Box>
